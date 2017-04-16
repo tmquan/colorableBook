37acc9688b415ebdee337700ab3d1c4c467dbc83
Tran Minh Quan <quantm@unist.ac.kr>
2017-04-16 22:30:03 +0900

    Unet startup code

---
+++ Unet.py
@@ -0,0 +1,14 @@
+from tensorpack import *
+import glob, os, natsort
+import cv2
+import skimage.io
+import matplotlib.pyplot as plt
+
+def get_data(): # Dataflow
+    pass
+
+def get_config(): #Trainer
+    pass
+
+class Unet(ModelDesc): #Model 
+    pass
